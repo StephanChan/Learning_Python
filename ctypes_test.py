5587b4da2b8a9233db7b56dc437f4c4ed6acd1eb
StephanChan <50417519+StephanChan@users.noreply.github.com>
2023-12-16 19:13:47 +0800

    Update ctypes_test.py

--- ctypes_test.py
+++ ctypes_test.py
@@ -44,7 +44,28 @@ for i in range(len(py_list)):
 d2[0]=c_list
 print(d2[0])
 
-
+#import ctypes 
+import ctypes 
+  
+  
+# variable declaration 
+val = [1, 2, 3, 4, 5] 
+  
+# display variable 
+print("Actual value -", val) 
+  
+# get the memory address of the python object  
+# for variable list 
+x = id(val) 
+  
+# display memory address 
+print("Memory address - ", x) 
+  
+# get the value through memory address 
+a = ctypes.cast(x, ctypes.py_object).value 
+  
+# display 
+print("Value - ", a) 
 
 
 
